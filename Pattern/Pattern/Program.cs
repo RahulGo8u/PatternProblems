d48b85d2d3c6c4f4186bd81344909cdf68072f63
rahulgo8u <rrsimt.rahul@gmail.com>
2023-08-02 13:55:26 +0400

    Pattern programs using c# | csharp

---
+++ Pattern/Pattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern1(4);
+        }
+        //*
+        //**
+        //***
+        //****
+        static void Pattern1(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(  "*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
